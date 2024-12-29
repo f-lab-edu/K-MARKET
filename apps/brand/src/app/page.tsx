126e08b248cbe225084e0f4186c3622f133e7bfc
김승훈 <rlatmdgns3076@gmail.com>
2024-12-30 05:46:20 +0900

    Revert "chore: supabase 설정 "

--- apps/brand/src/app/page.tsx
+++ apps/brand/src/app/page.tsx
@@ -1,12 +1,6 @@
 import { Button } from "@repo/ui/components/button";
-import { createClient } from "@/utils/supabase/server.ts";
 
-export default async function Home() {
-  /* TEST - Supabase로부터 사용자 정보를 가져옵니다. */
-  const supabase = await createClient();
-  const { data: users } = await supabase.from("users").select();
-  console.log(users)
-
+export default function Home() {
   return (
     <div>
       <Button size="sm" variant="destructive">
