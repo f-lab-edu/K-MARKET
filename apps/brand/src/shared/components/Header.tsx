8f6a96a5ce043d49bb0a926970b242b366bd85d3
김승훈 <rlatmdgns94@naver.com>
2025-01-11 06:35:16 +0900

    feat: Header

--- apps/brand/src/shared/components/Header.tsx
+++ apps/brand/src/shared/components/Header.tsx
@@ -5,7 +5,7 @@ interface TitleProps {
 }
 
 const Title = ({ title }: TitleProps) => {
-  return <h2 className="text-xl font-bold">{title}</h2>;
+  return <h2 className="text-2xl font-bold">{title}</h2>;
 };
 
 export default Title;
