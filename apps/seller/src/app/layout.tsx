89222149457eebdc9d069b772d77cb040e3ccfef
김승훈 <rlatmdgns94@naver.com>
2025-02-05 02:31:29 +0900

    feat: workspace seller 추가

---
+++ apps/seller/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+import "@repo/ui/globals.css";
+import { ThemeProvider } from "@/lib/ThemeProvider";
+import { cn } from "@repo/ui/lib/utils";
+import { Toaster } from "@repo/ui/components/toaster";
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="ko" suppressHydrationWarning>
+      <body
+        className={cn(
+          fontSans.variable,
+          fontMono.variable,
+          "font-sans antialiased",
+        )}
+      >
+        <ThemeProvider>{children}</ThemeProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
